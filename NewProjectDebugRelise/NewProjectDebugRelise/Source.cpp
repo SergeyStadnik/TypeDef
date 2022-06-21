2922c906a77adadd3185ea3a4358552bbfdaba85
SergeyStadnik <107566678+SergeyStadnik@users.noreply.github.com>
2022-06-21 19:59:30 +0300

    type def

--- NewProjectDebugRelise/NewProjectDebugRelise/Source.cpp
+++ NewProjectDebugRelise/NewProjectDebugRelise/Source.cpp
@@ -3,6 +3,8 @@
 #include<time.h>
 #include<stdlib.h>
 using namespace std;
+using si_t = short int;
+typedef unsigned long long l_t;
 /// <summary>
 /// Сумирование двух целых чисел
 /// </summary>
@@ -23,12 +25,19 @@ float sum(float a, float b)
 {
 	return a + b;
 }
+l_t sum(l_t a, l_t b)
+{
+	return a + b;
+}
+unsigned long long num1 = 101;
+l_t num = 1001;
+
 int main() {
 
 	setlocale(0, "");
 	srand(time(NULL));
 
-	sum
+	sum(2,2);
 
 
 	system("pause");
