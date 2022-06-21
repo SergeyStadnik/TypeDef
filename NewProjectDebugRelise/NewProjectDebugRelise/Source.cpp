ec03085543b10b574eb05e7873687ad9fe0e84f6
SergeyStadnik <107566678+SergeyStadnik@users.noreply.github.com>
2022-06-21 19:37:19 +0300

    add project

---
+++ NewProjectDebugRelise/NewProjectDebugRelise/Source.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<time.h>
+#include<stdlib.h>
+using namespace std;
+/// <summary>
+/// Сумирование двух целых чисел
+/// </summary>
+/// <param name="a">первое число</param>
+/// <param name="b">второе число</param>
+/// <returns>сумма а,b</returns>
+int sum(int a, int b)
+{
+	return a + b;
+}
+/// <summary>
+/// Сумирование двух дробных чисел
+/// </summary>
+/// <param name="a"></param>
+/// <param name="b"></param>
+/// <returns></returns>
+float sum(float a, float b)
+{
+	return a + b;
+}
+int main() {
+
+	setlocale(0, "");
+	srand(time(NULL));
+
+	sum
+
+
+	system("pause");
+	return 0;
+}
